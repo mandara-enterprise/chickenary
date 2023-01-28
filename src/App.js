9e517b6eded505d6cba0fba2a3c596cc8cca28ce
Arun MB <arunmb45@gmail.com>
2023-01-28 19:57:31 +0530

    title changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Chickenary website</p>
-        Learn to cook
+        Learn to cook with masterchef
       </header>
     </div>
   );
